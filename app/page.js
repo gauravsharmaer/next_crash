43ed2975674ea13213f10419f018463a5b2f695d
gauravsharmaer <gauravsharma701998@example.com>
2023-06-01 18:31:45 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ const HomePage = () => {
 
   return (
     <>
-      <h1>Welcome To Traversy Meida</h1>
+      <h1>Welcome To HomePage</h1>
        <CourseSearch getSearchResults={(results) => setCourses(results)} /> 
       <Courses courses={courses} />
     </>
